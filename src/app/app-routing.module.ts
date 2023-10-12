fbee9ba7fe3ee6baa6929537851cbc42beffc344
braga-gustavo <gustavo.b.moraes2@gmail.com>
2023-10-12 16:27:11 -0300

    menu routing and param getting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexModule } from './pages/index/index.module';
-import { PorfolioModule } from './pages/porfolio/porfolio.module';
+import { PorfolioModule } from './pages/porfolio/portfolio.module';
 import { TitleComponent } from './pages/index/title/title.component';
 import { CardComponent } from './pages/porfolio/card/card.component';
 
 const routes: Routes = [
-  {path: '', component: TitleComponent, pathMatch: 'full'},
-  {path: 'portfolio', component: CardComponent},
-  {path: '**', redirectTo: ''}
+  { path: '', component: TitleComponent, pathMatch: 'full' },
+  { path: 'portfolio/:id', component: CardComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
